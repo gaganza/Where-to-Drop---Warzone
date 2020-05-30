3abf2929131716572c1167ba0c24f11b228c51de
Gaston <gaston.aganza@gmail.com>
2020-05-29 21:38:06 -0700

    Add express server to feed bot info

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const auth = require("./auth.json");
+
+client.login(auth.token);
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.get("/", (req, res, next) => {
+  let data = client.toJSON();
+
+  res.send({
+    data,
+  });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
